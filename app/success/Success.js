103956e60bbcb1d96b10e78dd97efcff96fe7298
Tugulduroch <tkmusique.24@gmail.com>
2024-11-05 21:03:15 +0800

    success page added

---
+++ app/success/Success.js
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+const Success = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {" "}
+      <div className="rounded-lg p-10 max-w-lg text-center">
+        {" "}
+        <Image
+          src="/brand.png"
+          alt="Logo"
+          width={480}
+          height={180}
+          priority
+          className="mx-auto mb-6"
+        />
+        {/* <h1 className="text-3xl font-bold text-white mb-4">Thank You!</h1>{" "} */}
+        <p className="text-white mb-6">
+          Your message has been successfully sent. We appreciate your feedback
+          and will get back to you shortly!
+        </p>
+        <button
+          onClick={() => router.push("/")}
+          className="text-white px-6 py-2 rounded-lg underline "
+        >
+          Нүүр хуудас луу буцах
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Success;
--- app/success/page.js
+++ app/success/page.js
@@ -1,16 +1,17 @@
-"use client";
+// /app/success/page.js
+import Success from './success';
 
-import Fields from "./components/Fields"
-
-export default function Home() {
+const Page = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-4 px-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Fields/>
+        <Success/>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center px-4">
         Copyright © 2024 Ordinary - Work in Progress. All Rights Reserved.
       </footer>
     </div>
   );
-}
+};
+
+export default Page;
