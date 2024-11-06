61b4f20eee58d6de24f9de2bcbc36a5ad49473fb
Tugulduroch <tkmusique.24@gmail.com>
2024-11-06 08:04:28 +0800

    fix : minor

--- app/success/page.js
+++ app/success/page.js
@@ -1,5 +1,6 @@
 import Success from './Success';
 
+
 const Page = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-4 px-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
