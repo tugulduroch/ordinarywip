816387880cb4d4a4f5eb7ff7bcc42d5c87349e65
Tugulduroch <tkmusique.24@gmail.com>
2024-11-07 19:47:17 +0800

    fix : minor

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen py-4 px-4 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[#121212]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Home />
+        <HomePage />
       </main>
       <footer className="row-start-3 w-full flex gap-6 flex-wrap items-center justify-center px-4 text-white">
         Copyright © 2024 Ordinary - Work in Progress. All Rights Reserved.
